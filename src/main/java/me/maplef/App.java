524289d40f60ce6060c413cc22670b9f9618f1a9
Maplef <maplefsnow@gmail.com>
2024-12-30 09:06:23 +0000

    add unregister

--- src/main/java/me/maplef/App.java
+++ src/main/java/me/maplef/App.java
@@ -15,6 +15,19 @@ public class App
         try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
             botsApplication.registerBot(BOT_TOKEN, new NekoTownBot());
             System.out.println("NekoTownBot Hello World!");
+
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                public void run() {
+                    try {
+                        System.out.println("NekoTownBot GoodBye!");
+                        botsApplication.unregisterBot(BOT_TOKEN);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            });
+
             // Ensure this prcess wait forever
             Thread.currentThread().join();
         } catch (Exception e) {
